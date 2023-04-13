4c4f3201b4ae04fad3c8b5eb6f39a56570d78635
Abhishek Kumar <abhishekkumar.in047@gmail.com>
2023-04-13 14:27:44 +0530

    about component is updated

--- src/pages/components/about.js
+++ src/pages/components/about.js
@@ -6,11 +6,16 @@ import { SiWhatsapp,SiHtml5,SiCss3,SiJavascript,SiNodedotjs,SiNpm,SiYarn,SiBoots
 import Image from 'next/legacy/image';
 
 const About = () => {
+  const myString1 = " Hello, my name is Abhishek Kumar, and I am a self-taught frontend web developer. Despite graduating in Commerce, I found my true passion in programming and computers. Over the past year, I have been learning and practicing web development, and I've developed proficiency in HTML, CSS, JavaScript, Bootstrap, Tailwind CSS, React.js, Next.js, Git, GitHub, Node.js, NPM, and Yarn. I am constantly learning new technologies and tools to stay current with the latest trends in web development. ";
+
+  const myString2 = " I want to share my love of programming and technology with the world, but I do have a unique challenge that I face in my communication skills. I have a stutter that sometimes affects my speech, but I don't let that hold me back from pursuing my dreams. Despite this challenge, I am determined to build beautiful and functional websites that create engaging user experiences. ";
+  const myString3 = " I am a hard-working and dedicated individual, and I believe that my passion for programming and technology is what sets me apart from others in my field. I approach every project with enthusiasm and creativity, and I am always looking for new opportunities to challenge myself and grow as a developer. I am excited about the prospect of working with others who share my passion for technology and want to create amazing web experiences. If you're interested in working with me or want to learn more about my skills and experience, please don't hesitate to get in touch! My contact details are ";
+
   return (
     <div className='pro'>
     <Head><title>About Me</title></Head>
     <div className='flex flex-col md:px-20 md:text-lg sm:px-10 lg:px-32 lg:text-xl flex-wrap items-center p-3'>
-
+          
       <div >
           <Image
           layout='intrinsic'
@@ -18,16 +23,12 @@ const About = () => {
           height={750}
           className=' rounded-r-3xl rounded-bl-full rounded-tl-3xl'
             src='/images/meriphoto.jpg'
-            alt='abhishek-kumar'
+            alt={'abhishek-kumar'}
           />
       </div>
 
       <div className='text-center  mt-5'>
-      Hello, my name is Abhishek Kumar, and I am a self-taught frontend web developer. Despite graduating in Commerce, I found my true passion in programming and computers. Over the past year, I have been learning and practicing web development, and I've developed proficiency in HTML, CSS, JavaScript, Bootstrap, Tailwind CSS, React.js, Next.js, Git, GitHub, Node.js, NPM, and Yarn. I am constantly learning new technologies and tools to stay current with the latest trends in web development.<br/>
-
-I want to share my love of programming and technology with the world, but I do have a unique challenge that I face in my communication skills. I have a stutter that sometimes affects my speech, but I don't let that hold me back from pursuing my dreams. Despite this challenge, I am determined to build beautiful and functional websites that create engaging user experiences.<br/>
-
-I am a hard-working and dedicated individual, and I believe that my passion for programming and technology is what sets me apart from others in my field. I approach every project with enthusiasm and creativity, and I am always looking for new opportunities to challenge myself and grow as a developer. I am excited about the prospect of working with others who share my passion for technology and want to create amazing web experiences. If you're interested in working with me or want to learn more about my skills and experience, please don't hesitate to get in touch! My contact details are 
+        {myString1} <br /> {myString2} <br /> {myString3}
       </div>
       <div className='mt-10 flex flex-col items-center foot'>
           <div className='text-lg'>
